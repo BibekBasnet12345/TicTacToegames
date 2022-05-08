54a3781284b2a6fd7e987de9e4a3c760c00c7cc0
BibekBasnet12345 <bibekbas645@gmail.com>
2022-05-08 10:48:10 +0545

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/TictactoeGames/tictactoepackage/tictactoe.class

--- src/tictactoepackage/main.java
+++ src/tictactoepackage/main.java
@@ -2,7 +2,7 @@ package tictactoepackage;
 
 public class main {
     public static void main(String[] args) {
-        tictactoe Tictactoe=new tictactoe();
+        tictactoe Tictactoe = new tictactoe();
     }
 }
 
